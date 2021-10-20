ece9511d4a6d96286583f630e8982b04fc6f6052
JosphineSunday <54573870+JosphineSunday@users.noreply.github.com>
2021-10-21 00:10:38 +0100

    this program prints the elements in an index

---
+++ fun.py
@@ -0,0 +1,24 @@
+a = 3
+b = 4
+
+if a < b :print("a is greater than b")
+print("a is greater") if a > b else print("a is less")
+i = 6
+while i < 6:
+	print(i)
+
+i = 1
+while i < 11:
+  print(i)
+  i += 1
+
+i = 2000
+while i < 3200:
+	i +=1
+	if i % 7== 0 and i % 5== 0:
+		print (i, end=" ,") 
+		pass
+	# else:
+	# 	print("not divisble")
+y = ["my","your", "my"]
+x = ["mango","banana","pawpaw"]
